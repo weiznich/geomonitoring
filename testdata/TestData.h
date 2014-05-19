4416e3f5f4d65ee3d4916140536aaef8ccd0ea55
Georg Semmler <georg_semmler_05@web.de>
2014-05-19 13:57:53 +0200

    Better testdata generation

--- testdata/TestData.h
+++ testdata/TestData.h
@@ -12,6 +12,7 @@
 #include <pcl/point_cloud.h>
 
 #include <boost/random.hpp>
+#include <boost/random/random_device.hpp>
 
 #include <cmath>
 #include <iostream>
@@ -29,42 +30,44 @@ public:
 		for(auto p:source){
 			targetCloud.push_back(p);
 		}
-		Eigen::Matrix3f rot;
-		float alpha = randomFloat(-1, 1);
+		Eigen::Matrix4f rot;
+		float alpha = randomFloat(-0.1,0.1);
+		std::cout<<"alpha "<<alpha<<std::endl;
 		float n[3]={0,0,0};
 		float d[3]={0,0,0};
 		randomUnitVector(n);
 		randomUnitVector(d);
 		rot << n[1] * n[1] * (1 - cos(alpha)) + (cos(alpha)), n[1] * n[2]
 				* (1 - cos(alpha)) - n[3] * sin(alpha), n[1] * n[3]
-				* (1 - cos(alpha)) + n[2] * sin(alpha), n[2] * n[1]
+				* (1 - cos(alpha)) + n[2] * sin(alpha),d[0], n[2] * n[1]
 				* (1 - cos(alpha)) + n[3] * sin(alpha), n[2] * n[2]
 				* (1 - cos(alpha)) + cos(alpha), n[2] * n[3] * (1 - cos(alpha))
-				- n[1] * sin(alpha), n[3] * n[1] * (1 - cos(alpha))
+				- n[1] * sin(alpha),d[1], n[3] * n[1] * (1 - cos(alpha))
 				- n[2] * sin(alpha), n[3] * n[2] * (1 - cos(alpha))
-				+ n[1] * sin(alpha), n[3] * n[3] * (1 - cos(alpha)) + cos(alpha);
+				+ n[1] * sin(alpha), n[3] * n[3] * (1 - cos(alpha)) + cos(alpha),d[2],0,0,0,1;
 		std::cout << "Rotation: " << std::endl << rot << std::endl;
 		std::cout << "Translation: " << d[0] << " , " << d[1] << " , " << d[2]
 				<< std::endl;
 		for (auto& p : targetCloud) {
-			auto vector = p.getVector3fMap();
-			vector = rot * vector;
-			p.x = vector[0] + d[0];
-			p.y = vector[1] + d[1];
-			p.z = vector[2] + d[2];
+			Eigen::Vector4f norm;
+			norm<<p.x,p.y,p.z,1;
+			norm=rot*norm;
+			p.x = norm[0];
+			p.y = norm[1];
+			p.z = norm[2];
 		}
-		trafo = Eigen::Matrix4f();
-		trafo << rot.col(0)(0), rot.col(0)(1), rot.col(0)(2), d[0], rot.col(1)(0), rot.col(
-				1)(1), rot.col(1)(2), d[1], rot.col(2)(0), rot.col(2)(1), rot.col(2)(2), d[2], 0, 0, 0, 1;
-		std::cout<<"Trafo: "<<std::endl<<trafo<<std::endl;
+
+		std::cout<<"Trafo: "<<std::endl<<rot<<std::endl;
 	}
 private:
 	static float randomFloat(float min, float max)
 	{
 		boost::mt19937 rng;
+
 		boost::uniform_real<float> u(min, max);
 		boost::variate_generator<boost::mt19937&, boost::uniform_real<float> > gen(
 				rng, u);
+		rng.seed(boost::random::random_device()());
 		return gen();
 	}
 
@@ -73,10 +76,14 @@ private:
 		unit[0] = randomFloat(-10, 10);
 		unit[1]= randomFloat(-10, 10);
 		unit[2] = randomFloat(-10, 10);
+		std::cout<<unit[0]<<","<<unit[1]<<","<<unit[2]<<std::endl;
 		float abs = sqrtf(unit[0] * unit[0] +unit[1] * unit[1] + unit[2] * unit[2]);
+		std::cout<<"abs: "<<abs<<std::endl;
 		unit[0]/=abs;
+		unit[1]/=abs;
 		unit[2]/=abs;
-		unit[3]/=abs;
+		abs = sqrtf(unit[0] * unit[0] +unit[1] * unit[1] + unit[2] * unit[2]);
+		std::cout<<"abs "<<abs<<std::endl;
 	}
 };
 }
